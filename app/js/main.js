36d5e3b6c5f021ffb0d8179206fafbdaa552af5a
sergey-komar <kursev81@yandex.ru>
2024-12-17 19:30:03 +0300

    mobile work

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,39 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         appendArrows: '.full-slider__arrows',
-    })     
+        responsive: [
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 590,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+              },
+            },
+          ],
+    })  
+    
+    
+    $('.parents-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        appendArrows: '.parents__arrows',
+        // centerMode: true
+    })   
+    
+    $('.questions-accardion__btn').on('click', function(){
+        $(this).next().slideToggle(500); 
+     });
 })
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
    const card = document.querySelector('.card');
@@ -34,5 +65,93 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     
 
+    const accardionBtn = document.querySelectorAll('.questions-accardion__btn');
 
-})
+        accardionBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                item.classList.toggle('btn-active');
+            })
+        })
+
+
+
+    const modalBtn = document.querySelectorAll('.btn-lend');
+    const modal = document.querySelector('.modal');
+    const modalCloseBtn = document.querySelector('.modal-form__close');
+    const test = calcScroll();
+
+
+    if(modalBtn){
+        modalBtn.forEach(btn =>{
+            btn.addEventListener('click', ()=>{
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${test}px`;
+            })
+        });
+    }
+    
+    if(modalCloseBtn){
+        modalCloseBtn.addEventListener('click', ()=>{
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        });
+
+    }
+    
+    if(modal){
+        modal.addEventListener('click', (e)=>{
+            if(e.target == modal){
+                modal.classList.add('hide');
+                modal.classList.remove('show');
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+            }
+        });
+    }
+   
+
+    document.addEventListener('keydown', (e)=>{
+        if(e.code == 'Escape' && modal.classList.contains('show')){
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        }
+    })
+
+        // //УБИРАЕМ ДЁРГАНИЕ МОДАЛЬНОГО ОКНА ПРИ ПОЯВЛЕНИИ
+        function calcScroll(){
+            let div = document.createElement('div');
+            div.style.width = '50px';
+            div.style.height = '50px';
+            div.style.overflowY = 'scroll';
+            div.style.visibility = 'hidden';
+            
+            document.body.append(div);
+
+            let scrollWidth = div.offsetWidth - div.clientWidth;
+            
+            div.remove();
+            
+            return scrollWidth;
+            }
+            //УБИРАЕМ ДЁРГАНИЕ МОДАЛЬНОГО ОКНА ПРИ ПОЯВЛЕНИИ
+
+
+            const menu = document.querySelector(".menu");
+            const mobile = document.querySelector(".nav-icon");
+          
+            if(mobile){
+                mobile.addEventListener("click", function () {
+                    this.classList.toggle("nav-icon--active");
+                    menu.classList.toggle("nav--active");
+                  });
+            }
+           
+
+
+})//
